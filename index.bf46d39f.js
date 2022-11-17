c1f9aa5e97bc8f01c2bff0ec390fd564c4b5f2d3
SofiiaShevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-17 13:09:07 +0000

    Deploying to gh-pages from @ SofiiaShevtsova/my-project-js@ec37a5d443b5d30421314011ff101a3169126152 🚀

---
+++ index.bf46d39f.js
@@ -0,0 +1,2 @@
+document.querySelector(".hero--section");
+//# sourceMappingURL=index.bf46d39f.js.map
---
+++ index.cc7d3e0c.js
@@ -0,0 +1,2 @@
+document.querySelector(".hero--section");
+//# sourceMappingURL=index.cc7d3e0c.js.map
